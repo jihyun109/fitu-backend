23a3f97d03894ff6bc0583a97ec3fccb27b7d5da
jihyun109 <wlguswlgus989@gmail.com>
2025-04-30 09:15:23 +0900

    feat: 운동 부위 기반 루틴 추천 API 기본 구조 정의 및 구성 요소 생성 - RoutineRecommendationRequestDTO, RoutineRecommendationResponseDTO 추가 - WorkoutController, WorkoutService, WorkoutServiceImpl 생성

---
+++ src/main/java/com/hsp/fitu/dto/RoutineRecommendationRequestDTO.java
@@ -0,0 +1,9 @@
+package com.hsp.fitu.dto;
+
+import com.hsp.fitu.entity.enums.BodyPart;
+
+import java.util.List;
+
+public class RoutineRecommendationRequestDTO {
+    private List<BodyPart> bodyPartList;
+}
