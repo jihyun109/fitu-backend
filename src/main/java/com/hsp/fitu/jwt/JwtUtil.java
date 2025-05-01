17d3924cecbc33ed5b0f75a677965f5323c52631
jihyun109 <wlguswlgus989@gmail.com>
2025-05-01 22:26:30 +0900

    refactor: 인증 및 JWT 관련 로직 개선 - AuthController, AuthService 리팩토링 - reissue, logout API 추가 - JwtUtil refreshToken 기능 추가 - UnauthorizedException, TokenResponseDTO 클래스 추가

--- src/main/java/com/hsp/fitu/jwt/JwtUtil.java
+++ src/main/java/com/hsp/fitu/jwt/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.hsp.fitu.jwt;
 
 import com.hsp.fitu.entity.enums.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,13 +18,16 @@ import java.util.Map;
 public class JwtUtil {
     private final SecretKey secretKey;
     private final Long accessExpMs;
+    private final Long refreshExpMs;
 
     public JwtUtil(
             // 해당 @Value 값들은 yml에서 설정할 수 있다
             @Value("${jwt.secret}") String secret,
-            @Value("${jwt.token.access-expiration-time}") Long access) {
+            @Value("${jwt.token.access-expiration-time}") Long access,
+            @Value("${jwt.token.refresh-expiration-time}") Long refresh) {
         secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
         accessExpMs = access;
+        refreshExpMs = refresh;
     }
 
     public String createAccessToken(long userId, Role role) {
@@ -37,5 +42,36 @@ public class JwtUtil {
                 .setExpiration(Date.from(expiration))
                 .signWith(secretKey)
                 .compact();
+    }
+
+    public String createRefreshToken(long userId) {
+        Instant now = Instant.now();
+        Instant expiration = now.plusMillis(refreshExpMs);
+
+        return Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .addClaims(Map.of("userId", userId))
+                .setIssuedAt(Date.from(now))
+                .setExpiration(Date.from(expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("userId", Long.class);
     }
 }
--- src/main/java/com/hsp/fitu/service/AuthService.java
+++ src/main/java/com/hsp/fitu/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.hsp.fitu.service;
 
 import com.hsp.fitu.dto.LoginDTO;
+import com.hsp.fitu.dto.TokenResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
+
 
 public interface AuthService {
     LoginDTO oAuthLogin(String accessCode, HttpServletResponse httpServletResponse);
+    TokenResponseDTO reissue(String refreshToken);
 }
