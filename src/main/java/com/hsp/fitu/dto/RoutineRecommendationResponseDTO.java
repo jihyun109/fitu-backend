e7f4c08f4c42c952a50c1c72856dcab89102f8a9
jihyun109 <wlguswlgus989@gmail.com>
2025-06-13 22:45:30 +0900

    feat: 루틴추천 시 운동 부위도 함꼐 response하게 변경

--- src/main/java/com/hsp/fitu/dto/RoutineRecommendationResponseDTO.java
+++ src/main/java/com/hsp/fitu/dto/RoutineRecommendationResponseDTO.java
@@ -1,5 +1,6 @@
 package com.hsp.fitu.dto;
 
+import com.hsp.fitu.entity.enums.WorkoutCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import java.util.List;
 @Builder
 @Getter
 public class RoutineRecommendationResponseDTO {
+    private WorkoutCategory bodyPart;
     private WorkoutWithImageDTO mainWorkout;
     private List<WorkoutWithImageDTO> similarWorkouts;
 }
