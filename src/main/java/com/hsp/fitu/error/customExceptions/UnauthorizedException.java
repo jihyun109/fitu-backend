9c9787ae25cb59b891883623c0b30296e5815cf4
jihyun109 <wlguswlgus989@gmail.com>
2025-07-29 12:50:18 +0900

    [backend] refactor: UnauthorizedException에 ErrorCode 기반 생성자 및 @Getter 추가

--- src/main/java/com/hsp/fitu/error/customExceptions/UnauthorizedException.java
+++ src/main/java/com/hsp/fitu/error/customExceptions/UnauthorizedException.java
@@ -1,11 +1,19 @@
 package com.hsp.fitu.error.customExceptions;
 
 import com.hsp.fitu.error.ErrorCode;
+import lombok.Getter;
 
-public class UnauthorizedException extends RuntimeException{
+@Getter
+public class UnauthorizedException extends RuntimeException {
     private ErrorCode errorCode;
-    public UnauthorizedException(String message, ErrorCode errorCode){
+
+    public UnauthorizedException(String message, ErrorCode errorCode) {
         super(message);
+        this.errorCode = errorCode;
+    }
+
+    public UnauthorizedException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 }
