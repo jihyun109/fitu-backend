8f2dfd4c8fed8e68cdedec2be8b66cf9cda56cdf
jihyun109 <wlguswlgus989@gmail.com>
2025-07-29 12:57:04 +0900

    [backend] feat: 사용자 및 운동 도메인의 NotFoundException 커스텀 예외 추가

---
+++ src/main/java/com/hsp/fitu/error/customExceptions/UserNotFoundException.java
@@ -0,0 +1,19 @@
+package com.hsp.fitu.error.customExceptions;
+
+import com.hsp.fitu.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class UserNotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public UserNotFoundException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public UserNotFoundException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
