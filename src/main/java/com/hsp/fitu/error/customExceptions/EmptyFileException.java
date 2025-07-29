2eafd71f05ef37922f6ccd66be52c982f0c20923
jihyun109 <wlguswlgus989@gmail.com>
2025-07-29 13:15:03 +0900

    [backend] refactor: 커스텀 예외 생성자 message 파라미터 제거 및 ErrorCode 기반으로 통일

--- src/main/java/com/hsp/fitu/error/customExceptions/EmptyFileException.java
+++ src/main/java/com/hsp/fitu/error/customExceptions/EmptyFileException.java
@@ -2,10 +2,14 @@ package com.hsp.fitu.error.customExceptions;
 
 
 import com.hsp.fitu.error.ErrorCode;
+import lombok.Getter;
 
-public class EmptyFileException extends RuntimeException{
-    private ErrorCode errorCode;
-    public EmptyFileException(ErrorCode errorCode){
+@Getter
+public class EmptyFileException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public EmptyFileException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 }
