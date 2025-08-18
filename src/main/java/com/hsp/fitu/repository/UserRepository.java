541722c6ed50b22eb3a61fc9bd9d71bcbe80570e
jihyun109 <wlguswlgus989@gmail.com>
2025-08-19 08:27:26 +0900

    feat: UserRepository에 friendCode로 사용자 id 조회 메서드 추가

--- src/main/java/com/hsp/fitu/repository/UserRepository.java
+++ src/main/java/com/hsp/fitu/repository/UserRepository.java
@@ -2,10 +2,16 @@ package com.hsp.fitu.repository;
 
 import com.hsp.fitu.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByKakaoEmail(String kakaoEmail);
     Optional<UserEntity> findById(Long id);
+
+    // 친구 초대 코드로 사용자 id 조회
+    @Query("SELECT u.id FROM UserEntity u WHERE u.friendCode = :code")
+    Long findIdByFriendCode(@Param("code") String code);
 }
