28fc5214a0a9b1edac68c43a43e313b72aeb17fd
jihyun109 <wlguswlgus989@gmail.com>
2025-08-19 09:22:34 +0900

    feat: FriendController에 친구 추가 API 구현

--- src/main/java/com/hsp/fitu/service/FriendService.java
+++ src/main/java/com/hsp/fitu/service/FriendService.java
@@ -1,5 +1,7 @@
 package com.hsp.fitu.service;
 
+import com.hsp.fitu.dto.FriendAddRequestDTO;
+
 public interface FriendService {
-    void addFriend(String code, Long userId);
+    void addFriend(FriendAddRequestDTO code, Long userId);
 }
