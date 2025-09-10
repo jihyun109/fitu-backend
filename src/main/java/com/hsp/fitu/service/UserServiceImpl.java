26bbb17faa97f920353e684543dff1a401a2de6f
jihyun109 <wlguswlgus989@gmail.com>
2025-09-10 16:32:05 +0900

    feat: UserService 인터페이스 및 구현체 추가 & saveInfo 메서드 정의

---
+++ src/main/java/com/hsp/fitu/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hsp.fitu.service;
+
+import com.hsp.fitu.dto.UserInfoRequestDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public void saveInfo(Long userId, UserInfoRequestDTO userInfoRequestDTO) {
+
+    }
+}
