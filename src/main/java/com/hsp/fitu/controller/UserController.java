dba360cf750cc357cda89129d62316bea1a1e8de
jihyun109 <wlguswlgus989@gmail.com>
2025-09-16 15:06:27 +0900

    fix: UserController의 @Controller -> @RestController로 수정

--- src/main/java/com/hsp/fitu/controller/UserController.java
+++ src/main/java/com/hsp/fitu/controller/UserController.java
@@ -6,10 +6,9 @@ import com.hsp.fitu.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
