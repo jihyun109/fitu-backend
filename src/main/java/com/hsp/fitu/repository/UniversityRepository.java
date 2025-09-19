4fb3004f1881530f0f3c2133552b7886de93b853
jihyun109 <wlguswlgus989@gmail.com>
2025-09-19 10:36:42 +0900

    feat: 사용자 정보 저장 시 대학교 도메인 기반 universityId 매핑 기능 추가

--- src/main/java/com/hsp/fitu/repository/UniversityRepository.java
+++ src/main/java/com/hsp/fitu/repository/UniversityRepository.java
@@ -1,8 +1,13 @@
 package com.hsp.fitu.repository;
 
-import com.hsp.fitu.entity.PhysicalInfoEntity;
+import com.hsp.fitu.entity.UniversityEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface UniversityRepository extends JpaRepository<PhysicalInfoEntity, Long> {
+@Repository
+public interface UniversityRepository extends JpaRepository<UniversityEntity, Long> {
 
+    @Query("SELECT u.id FROM UniversityEntity u WHERE u.domainName = :domainName")
+    Long findIdByDomainName(String domainName);
 }
