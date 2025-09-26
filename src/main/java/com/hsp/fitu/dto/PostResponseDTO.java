8ed03eddd2ab38b9f48bb0c6c34668d77e4ee1c9
budoli <als01540@gmail.com>
2025-09-26 14:15:19 +0900

    feat : post 구현 및 테스트 완료

--- src/main/java/com/hsp/fitu/dto/PostResponseDTO.java
+++ src/main/java/com/hsp/fitu/dto/PostResponseDTO.java
@@ -1,19 +1,13 @@
 package com.hsp.fitu.dto;
 
 import com.hsp.fitu.entity.enums.PostCategory;
-import lombok.Builder;
-import lombok.Data;
 
 import java.time.LocalDateTime;
 
-@Data
-@Builder
-public class PostResponseDTO {
-    private Long id;
-    private PostCategory category;
-    private Long universityId;
-    private Long writerId;
-    private String title;
-    private String contents;
-    private LocalDateTime createdAt;
-}
+public record PostResponseDTO(
+      Long id,
+      PostCategory category,
+      String title,
+      String contents,
+       LocalDateTime createdAt
+) {}
