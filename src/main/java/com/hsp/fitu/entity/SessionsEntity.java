4935a44fde7e042e17e07dbe06669bbd0cc5a0ab
budoli <als01540@gmail.com>
2025-10-06 00:22:58 +0900

    feat : session엔티티, set 엔티티 구현

---
+++ src/main/java/com/hsp/fitu/entity/SessionsEntity.java
@@ -0,0 +1,23 @@
+package com.hsp.fitu.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sessions")
+public class SessionsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "user_id")
+    private long userId;
+
+    @Column(name = "exercise_image_id")
+    private long exerciseImageId;
+}
