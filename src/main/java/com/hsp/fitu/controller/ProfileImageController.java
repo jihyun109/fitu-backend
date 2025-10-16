d8141ad40fe20acdfa0d5a3ba4618059a22148b3
jihyun109 <wlguswlgus989@gmail.com>
2025-10-16 13:25:49 +0900

    refactor: BodyImageController,  BodyImageService, BodyImageServiceImpl 를 ProfileImageController, ProfileImageService, ProfileImageServiceImpl로 수정

--- src/main/java/com/hsp/fitu/controller/ProfileImageController.java
+++ src/main/java/com/hsp/fitu/controller/ProfileImageController.java
@@ -5,7 +5,7 @@ import com.hsp.fitu.dto.BodyImageMainResponseDTO;
 import com.hsp.fitu.dto.BodyImageUploadResponseDTO;
 import com.hsp.fitu.entity.BodyImageEntity;
 import com.hsp.fitu.jwt.CustomUserDetails;
-import com.hsp.fitu.service.BodyImageService;
+import com.hsp.fitu.service.ProfileImageService;
 import com.hsp.fitu.service.S3ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,24 +17,24 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/body-image")
+@RequestMapping("/profile-image")
 @RequiredArgsConstructor
 @Slf4j
-public class BodyImageController {
-    private final BodyImageService bodyImageService;
+public class ProfileImageController {
+    private final ProfileImageService profileImageService;
     private final S3ImageService s3ImageService;
 
     @GetMapping()
     public ResponseEntity<BodyImageMainResponseDTO> getMainBodyPhoto(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Long userId = userDetails.getId();
-        return ResponseEntity.ok(bodyImageService.getMainBodyImage(userId));
+        return ResponseEntity.ok(profileImageService.getMainBodyImage(userId));
     }
 
     @GetMapping("/history")
     public ResponseEntity<List<BodyImageEntity>> getBodyImages(@AuthenticationPrincipal CustomUserDetails userDetails) {
         Long userId = userDetails.getId();
 
-        List<BodyImageEntity> bodyImageEntityList = bodyImageService.getBodyImages(userId);
+        List<BodyImageEntity> bodyImageEntityList = profileImageService.getBodyImages(userId);
         return ResponseEntity.ok(bodyImageEntityList);
     }
 
