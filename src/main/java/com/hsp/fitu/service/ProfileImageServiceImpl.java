1401e2eeebf1ec941cc2370f5736e8a7c1b8cb29
jihyun109 <wlguswlgus989@gmail.com>
2025-10-16 13:25:49 +0900

    refactor: BodyImageController,  BodyImageService, BodyImageServiceImpl 를 ProfileImageController, ProfileImageService, ProfileImageServiceImpl로 수정

--- src/main/java/com/hsp/fitu/service/ProfileImageServiceImpl.java
+++ src/main/java/com/hsp/fitu/service/ProfileImageServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class BodyImageServiceImpl implements BodyImageService {
+public class ProfileImageServiceImpl implements ProfileImageService {
     private final BodyImageRepository bodyImageRepository;
 
     @Override
