301da4855d949ba21a2f682e4fc285522a5e935b
jihyun109 <wlguswlgus989@gmail.com>
2025-10-17 09:52:54 +0900

    refactor: profile image 관련 api 경로에 /api/v2 prefix 추가

--- src/main/java/com/hsp/fitu/controller/ProfileImageController.java
+++ src/main/java/com/hsp/fitu/controller/ProfileImageController.java
@@ -17,7 +17,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/profile-image")
+@RequestMapping("/api/v2/profile-image")
 @RequiredArgsConstructor
 @Slf4j
 public class ProfileImageController {
