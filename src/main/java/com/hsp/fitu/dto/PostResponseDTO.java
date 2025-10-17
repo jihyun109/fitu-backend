8892d027665add1c4b2d17c0ddba82b4f826f94d
budoli <als01540@gmail.com>
2025-10-17 15:06:21 +0900

    ref: 단일 게시물 조회에 카테고리 추가

--- src/main/java/com/hsp/fitu/dto/PostResponseDTO.java
+++ src/main/java/com/hsp/fitu/dto/PostResponseDTO.java
@@ -1,9 +1,12 @@
 package com.hsp.fitu.dto;
 
+import com.hsp.fitu.entity.enums.PostCategory;
+
 import java.time.LocalDateTime;
 
 public record PostResponseDTO(
       long id,
+      PostCategory postCategory,
       String title,
       String writerName,
       String writerProfileImgUrl,
