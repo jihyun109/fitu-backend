fe58dc5b032c7c03425938f940fd42e738a98961
jihyun109 <wlguswlgus989@gmail.com>
2025-10-20 18:37:28 +0900

    refactor(workout-verification-request): MediaFilesEntity에 @Builder, @NoArgsConstructor, @AllArgsConstructor 어노테이션 추가

--- src/main/java/com/hsp/fitu/entity/MediaFilesEntity.java
+++ src/main/java/com/hsp/fitu/entity/MediaFilesEntity.java
@@ -4,14 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Entity
+@Builder
 @Table(name = "media_files")
+@NoArgsConstructor
+@AllArgsConstructor
 public class MediaFilesEntity {
     @Id
     private long id;
