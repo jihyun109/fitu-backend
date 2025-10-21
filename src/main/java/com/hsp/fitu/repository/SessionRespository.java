aeaa10d0de152209275db6bcb4c465f3232606e7
jihyun109 <wlguswlgus989@gmail.com>
2025-10-21 18:49:37 +0900

    feat(ranking): 규칙적 운동 랭킹 조회 api에 나의 랭킹 조회 로직 구형 및 적용

--- src/main/java/com/hsp/fitu/repository/SessionRespository.java
+++ src/main/java/com/hsp/fitu/repository/SessionRespository.java
@@ -26,4 +26,29 @@ public interface SessionRespository extends JpaRepository<SessionsEntity, Long> 
             LIMIT 6
             """, nativeQuery = true)
     List<RankingItem> findAllRankingWorkoutCountByUserId(Long userId);
+
+    @Query(value = """
+            WITH ranked_users AS (
+            	SELECT
+            		ROW_NUMBER() OVER (ORDER BY COUNT(DISTINCT s.id) DESC) AS ranking,
+            		u.id AS userId,
+            		u.name AS userName,
+            		COUNT(s.id) AS amount,
+            		COALESCE(
+            			CASE WHEN u.profile_visibility = FALSE THEN NULL ELSE m.url END,
+            				'https://fitu-bucket.s3.ap-northeast-2.amazonaws.com/fitu_default_image.png'
+            			) AS profileImageUrl
+            	FROM sessions s
+            	JOIN users u ON s.user_id = u.id
+            	LEFT JOIN media_files m ON u.profile_img_id = m.id
+            	WHERE u.university_id = (SELECT university_id FROM users u2 WHERE u2.id = :userId)
+            		AND s.start_time BETWEEN DATE_FORMAT(CURDATE(), '%Y-%m-01') AND LAST_DAY(CURDATE())
+            	GROUP BY u.id
+            	ORDER BY ranking
+            )
+            SELECT *
+            FROM ranked_users
+            WHERE userId = :userId
+            """, nativeQuery = true)
+    RankingItem findRankingWorkoutCountByUserId(Long userId);
 }
