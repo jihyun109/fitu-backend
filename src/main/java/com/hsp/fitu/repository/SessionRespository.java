dc5e1e795268f7dc8dbcac3e597e31a5a28561a0
jihyun109 <wlguswlgus989@gmail.com>
2025-10-21 16:43:10 +0900

    feat(ranking): 규칙적 운동 랭킹 조회 api 구현을 위해 응답 dto RankingWorkoutCountResponseDTO 생성

---
+++ src/main/java/com/hsp/fitu/repository/SessionRespository.java
@@ -0,0 +1,8 @@
+package com.hsp.fitu.repository;
+
+import com.hsp.fitu.entity.SessionsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRespository extends JpaRepository<SessionsEntity, Long> {
+
+}
