86c8058a1bcc59aebd47dbed3c6bb556e3633942
jihyun109 <wlguswlgus989@gmail.com>
2025-10-21 16:43:10 +0900

    refactor(ranking): 규칙적 운동 랭킹 조회 api 구현을 위해 응답 dto RankingWorkoutCountResponseDTO 생성

---
+++ src/main/java/com/hsp/fitu/dto/RankingWorkoutCountResponseDTO.java
@@ -0,0 +1,8 @@
+package com.hsp.fitu.dto;
+
+import java.util.List;
+
+public class RankingWorkoutCountResponseDTO {
+    private List<RankingItem> rankingItems;
+    private RankingItem myRanking;
+}
