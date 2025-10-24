effd45b934fb8bfaeeacfc2e4e8db7aacb3c0ec6
budoli <als01540@gmail.com>
2025-10-25 01:25:11 +0900

    feat: 관리자 게시물 관리

---
+++ src/main/java/com/hsp/fitu/service/AdminPostManagementServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hsp.fitu.service;
+
+import com.hsp.fitu.dto.AdminPostManagementRequestDTO;
+import com.hsp.fitu.dto.AdminPostManagementResponseDTO;
+import com.hsp.fitu.dto.PostSliceResponseDTO;
+import com.hsp.fitu.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AdminPostManagementServiceImpl implements AdminPostManagementService{
+    private final PostRepository postRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public PostSliceResponseDTO<AdminPostManagementResponseDTO> getPostsByUniversity(AdminPostManagementRequestDTO requestDTO, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+
+        Page<AdminPostManagementResponseDTO> resultPage =
+                postRepository.findPostsByUniversityName(requestDTO.universityName(), pageRequest);
+
+        return new PostSliceResponseDTO<>(
+                requestDTO.universityName(),
+                resultPage.getContent(),
+                resultPage.hasNext()
+        );
+    }
+}
