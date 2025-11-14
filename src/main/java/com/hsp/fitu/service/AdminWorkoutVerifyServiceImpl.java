5c4adf6808c9870faeb3da46838542ab5f33b02d
budoli <als01540@gmail.com>
2025-11-14 14:17:53 +0900

    feat : admin 운동인증 기능 구현시작

---
+++ src/main/java/com/hsp/fitu/service/AdminWorkoutVerifyServiceImpl.java
@@ -0,0 +1,36 @@
+package com.hsp.fitu.service;
+
+import com.hsp.fitu.entity.WorkoutVerificationEntity;
+import com.hsp.fitu.entity.enums.WorkoutVerificationRequestStatus;
+import com.hsp.fitu.repository.AdminWorkoutVerifyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminWorkoutVerifyServiceImpl implements AdminWorkoutVerifyService{
+    private final AdminWorkoutVerifyRepository adminWorkoutVerifyRepository;
+
+    @Transactional(readOnly = true)
+    public List<WorkoutVerificationEntity> getPendingVerify() {
+        return adminWorkoutVerifyRepository.findAllByStatus(WorkoutVerificationRequestStatus.PENDING);
+    }
+
+    @Transactional(readOnly = true)
+    public WorkoutVerificationEntity getVerifyDetail(Long id) {
+        return adminWorkoutVerifyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Verification not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public void approveVerify(Long id, int weight) {
+        WorkoutVerificationEntity entity = adminWorkoutVerifyRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Verification not found"));
+
+    }
+}
