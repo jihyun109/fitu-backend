6dcb1b6e5c4321faf3d7fe5eef0df30096a59636
budoli <als01540@gmail.com>
2025-12-07 19:16:09 +0900

    feat: 마이페이지 캘린더 운동 조회/ 세부 조회 구현

--- src/main/java/com/hsp/fitu/controller/WorkoutCalendarController.java
+++ src/main/java/com/hsp/fitu/controller/WorkoutCalendarController.java
@@ -1,8 +1,10 @@
 package com.hsp.fitu.controller;
 
 import com.hsp.fitu.dto.WorkoutCalendarFullDTO;
+import com.hsp.fitu.dto.WorkoutCalendarSummaryDTO;
 import com.hsp.fitu.jwt.CustomUserDetails;
 import com.hsp.fitu.service.WorkoutCalendarService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -20,8 +23,9 @@ public class WorkoutCalendarController {
 
     private final WorkoutCalendarService workoutCalendarService;
 
+    @Operation(summary = "마이페이지 해당 날짜 운동 조회 by 조민기")
     @GetMapping("/full")
-    public ResponseEntity<List<WorkoutCalendarFullDTO>> getFullWorkoutCalendar(
+    public ResponseEntity<List<WorkoutCalendarSummaryDTO>> getFullWorkoutCalendar(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestParam int year,
             @RequestParam int month) {
@@ -29,5 +33,17 @@ public class WorkoutCalendarController {
         return ResponseEntity.ok(
                 workoutCalendarService.getFullWorkoutCalendar(userId, year, month)
         );
+    }
+
+    @Operation(summary = "마이페이지_운동기록 상세 by 조민기")
+    @GetMapping("/details")
+    public ResponseEntity<WorkoutCalendarFullDTO> getDetailWorkoutCalendar(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @RequestParam LocalDate date) {
+
+        Long userId = userDetails.getId();
+        return ResponseEntity.ok(
+                workoutCalendarService.getDetailWorkoutCalendar(userId, date));
+
     }
 }
--- src/main/java/com/hsp/fitu/dto/WorkoutCalendarFullDTO.java
+++ src/main/java/com/hsp/fitu/dto/WorkoutCalendarFullDTO.java
@@ -1,14 +1,10 @@
 package com.hsp.fitu.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDate;
 import java.util.List;
 
-@Getter
-@AllArgsConstructor
-public class WorkoutCalendarFullDTO {
-    private LocalDate date;
-    private List<WorkoutCalendarDetailDTO> details;
-}
+public record WorkoutCalendarFullDTO(
+        LocalDate date,
+        int totalMinutes,
+        List<WorkoutCalendarDetailDTO> details
+) {}
