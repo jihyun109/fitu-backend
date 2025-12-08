77807940ef8c4e7ba4d332a43f274472daf3289c
jihyun109 <wlguswlgus989@gmail.com>
2025-12-08 16:49:04 +0900

    refactor: WebSocketConfig 디렉토리 이동

--- src/main/java/com/hsp/fitu/config/websocket/WebSocketConfig.java
+++ src/main/java/com/hsp/fitu/config/websocket/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.hsp.fitu.config;
+package com.hsp.fitu.config.websocket;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
