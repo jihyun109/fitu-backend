f83f677a9f4323eb93f160510003afc92c64ef62
jihyun109 <wlguswlgus989@gmail.com>
2025-12-08 16:46:47 +0900

    feat: WebSocket, STOMP 기본 설정 추가

---
+++ src/main/java/com/hsp/fitu/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package com.hsp.fitu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 클라이언트가 최초 WebSocket 연결을 시도할 엔드포인트를 등록
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws/chat")    // 웹소켓 연결 주소를 /ws/chat 로 만듦
+                .setAllowedOriginPatterns("*")      // cors 허용
+                .withSockJS();                      // 웹소켓을 지원하지 않는 브라우저를 위해 SockJS 사용
+    }
+
+    // STOMP 메시지가 들어가고, 어디로 나가는 곳을 지정하는 라우팅 규칙 설정
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/pub");         // 클라이언트 → 서버 방향 메시지(prefix)
+        registry.enableSimpleBroker("/sub");       // 서버 → 클라이언트 방향 메시지(prefix)
+    }
+}
+
