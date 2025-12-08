49bd03450ae8c6503941a4c1473fb392a72c42aa
jihyun109 <wlguswlgus989@gmail.com>
2025-12-08 17:22:26 +0900

    feat: WebSocketConfig에 registerStompEndpoints에서 interceptor 등록

--- src/main/java/com/hsp/fitu/config/websocket/WebSocketConfig.java
+++ src/main/java/com/hsp/fitu/config/websocket/WebSocketConfig.java
@@ -1,19 +1,25 @@
 package com.hsp.fitu.config.websocket;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final WebSocketAuthInterceptor webSocketAuthInterceptor;
 
     // 클라이언트가 최초 WebSocket 연결을 시도할 엔드포인트를 등록
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")    // 웹소켓 연결 주소를 /ws/chat 로 만듦
+                .addInterceptors(webSocketAuthInterceptor)
                 .setAllowedOriginPatterns("*")      // cors 허용
                 .withSockJS();                      // 웹소켓을 지원하지 않는 브라우저를 위해 SockJS 사용
     }
