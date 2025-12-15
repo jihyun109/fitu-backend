02da1f59c85352a0df1d0ba063fc67f6a20eab03
jihyun109 <wlguswlgus989@gmail.com>
2025-12-16 01:59:20 +0900

    feat: 커스텀하기 운동 infos 조회

--- src/main/java/com/hsp/fitu/repository/WorkoutRepository.java
+++ src/main/java/com/hsp/fitu/repository/WorkoutRepository.java
@@ -1,7 +1,9 @@
 package com.hsp.fitu.repository;
 
+import com.hsp.fitu.dto.SelectedWorkout;
 import com.hsp.fitu.entity.WorkoutEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,13 @@ import java.util.List;
 @Repository
 public interface WorkoutRepository extends JpaRepository<WorkoutEntity, Long> {
     List<WorkoutEntity> findAllByCategoryId(@Param("categoryId") Long categoryId);
+
+    @Query("""
+            SELECT new com.hsp.fitu.dto.SelectedWorkout(
+                w.id, w.workoutName, w.workoutDescription, w.imageUrl, w.gifUrl
+            )
+            FROM WorkoutEntity w
+            WHERE w.id IN :ids
+            """)
+    List<SelectedWorkout> findAllByIds(@Param("ids") List<Long> ids);
 }
