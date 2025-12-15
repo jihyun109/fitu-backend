909c979573723cd8f5a0ba9d66f2e98552d98961
jihyun109 <wlguswlgus989@gmail.com>
2025-12-16 01:09:10 +0900

    fix: 채팅방 생성 오류

--- src/main/java/com/hsp/fitu/entity/ChatRoomMemberEntity.java
+++ src/main/java/com/hsp/fitu/entity/ChatRoomMemberEntity.java
@@ -14,6 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChatRoomMemberEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long chatRoomId;
     private Long userId;
