dc4c5f27dc1c925b5c045e76b36a6f05b8ceb507
jihyun109 <wlguswlgus989@gmail.com>
2025-12-15 21:36:33 +0900

    feat: 채팅방, 채팅방 멤버 관련 entity, repository 생성

--- src/main/java/com/hsp/fitu/entity/ChatRoomEntity.java
+++ src/main/java/com/hsp/fitu/entity/ChatRoomEntity.java
@@ -3,11 +3,17 @@ package com.hsp.fitu.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "chat_rooms")
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatRoomEntity {
     @Id
     private Long id;
--- src/main/java/com/hsp/fitu/entity/ChatRoomMemberEntity.java
+++ src/main/java/com/hsp/fitu/entity/ChatRoomMemberEntity.java
@@ -1,11 +1,17 @@
 package com.hsp.fitu.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "chat_room_members")
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChatRoomMemberEntity {
     @Id
     private Long id;
