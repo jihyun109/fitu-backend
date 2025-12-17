b521571e201ce47796c0c3f313da027f009403fd
jihyun109 <wlguswlgus989@gmail.com>
2025-12-17 10:19:33 +0900

    feat: 사용자의 채팅방 리스트 조회 쿼리

--- src/main/java/com/hsp/fitu/repository/ChatRoomRepository.java
+++ src/main/java/com/hsp/fitu/repository/ChatRoomRepository.java
@@ -1,10 +1,34 @@
 package com.hsp.fitu.repository;
 
+import com.hsp.fitu.dto.ChatRoom;
 import com.hsp.fitu.entity.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
+
+    @Query("""
+            SELECT new com.hsp.fitu.dto.ChatRoom(
+                cr.id,
+                cr.roomName,
+                (   SELECT cm.content
+                    FROM ChatMessageEntity cm
+                    WHERE cm.chatRoomId = cr.id
+                    ORDER BY cm.createdAt DESC
+                    LIMIT 1
+                ),
+                mf.url)
+            FROM ChatRoomEntity cr
+            JOIN ChatRoomMemberEntity crm ON cr.id = crm.chatRoomId
+            LEFT JOIN MediaFilesEntity mf ON cr.thumbnailImgId = mf.id
+            WHERE crm.userId = :userId
+            """)
+    List<ChatRoom> getChatRoomList(@Param("userId") Long userId);
 
 }
