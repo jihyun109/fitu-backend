c4e546e9cd2296641e09a7ce668b4e157d5bf7a6
jihyun109 <wlguswlgus989@gmail.com>
2025-12-17 10:20:40 +0900

    feat: 채팅방 생성 시 채팅방 대표 img id 함꼐 저장, 사용자의 채팅방 list 조회

--- src/main/java/com/hsp/fitu/controller/ChatRoomController.java
+++ src/main/java/com/hsp/fitu/controller/ChatRoomController.java
@@ -2,16 +2,14 @@ package com.hsp.fitu.controller;
 
 import com.hsp.fitu.dto.ChatRoomCreateRequestDTO;
 import com.hsp.fitu.dto.ChatRoomCreateResponseDTO;
+import com.hsp.fitu.dto.ChatRoomListResponseDTO;
 import com.hsp.fitu.jwt.CustomUserDetails;
 import com.hsp.fitu.service.ChatRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +23,12 @@ public class ChatRoomController {
     public ResponseEntity<ChatRoomCreateResponseDTO> createChatRoom(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestBody ChatRoomCreateRequestDTO chatRoomCreateRequestDTO) {
 
         return ResponseEntity.ok(chatRoomService.createChatRoom(userDetails.getId(), chatRoomCreateRequestDTO));
+    }
+
+    @GetMapping("room/list")
+    @Operation(summary = "사용자의 채팅방 list 조회 by 장지현")
+    public ResponseEntity<ChatRoomListResponseDTO>  getChatRoomList(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(chatRoomService.getChatRoomList(userDetails.getId()));
     }
 
 }
