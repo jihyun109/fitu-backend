d6752d82bdf59955a73e7d3ea3d556e6eae0ecef
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 18:16:05 +0900

    fix: profile img 조회

--- src/main/java/com/hsp/fitu/repository/MediaFilesRepository.java
+++ src/main/java/com/hsp/fitu/repository/MediaFilesRepository.java
@@ -1,5 +1,6 @@
 package com.hsp.fitu.repository;
 
+import com.hsp.fitu.dto.BodyImageMainResponseDTO;
 import com.hsp.fitu.dto.ProfileImage;
 import com.hsp.fitu.entity.MediaFilesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,13 +14,24 @@ import java.util.List;
 @Repository
 public interface MediaFilesRepository extends JpaRepository<MediaFilesEntity, Long> {
     @Query("""
-            SELECT new com.hsp.fitu.dto.ProfileImage(m.url)
+            SELECT new com.hsp.fitu.dto.BodyImageMainResponseDTO(m.url)
             FROM MediaFilesEntity m
             JOIN UserEntity u ON m.id = u.profileImgId
             WHERE u.id = :userId
+            ORDER BY m.uploadedAt DESC 
+            LIMIT 1
+            """)
+    BodyImageMainResponseDTO findMainProfileImageByUserId(@Param("userId")long userId);
+
+
+    @Query("""
+            SELECT new com.hsp.fitu.dto.ProfileImage(m.url)
+            FROM MediaFilesEntity m
+            JOIN UserEntity u ON m.uploaderId = u.id
+            WHERE u.id = :userId
             ORDER BY m.uploadedAt
             """)
-    List<ProfileImage> findProfileImgsByUserId(long userId);
+    List<ProfileImage> findProfileImgsByUserId(@Param("userId") long userId);
 
 
     @Modifying
--- src/main/java/com/hsp/fitu/service/ProfileImageServiceImpl.java
+++ src/main/java/com/hsp/fitu/service/ProfileImageServiceImpl.java
@@ -2,11 +2,9 @@ package com.hsp.fitu.service;
 
 import com.hsp.fitu.dto.BodyImageMainResponseDTO;
 import com.hsp.fitu.dto.ProfileImagesResponseDTO;
-import com.hsp.fitu.entity.BodyImageEntity;
 import com.hsp.fitu.entity.MediaFilesEntity;
 import com.hsp.fitu.entity.enums.MediaCategory;
 import com.hsp.fitu.entity.enums.MediaType;
-import com.hsp.fitu.repository.BodyImageRepository;
 import com.hsp.fitu.repository.MediaFilesRepository;
 import com.hsp.fitu.repository.UserRepository;
 import com.hsp.fitu.validator.MediaValidator;
@@ -20,18 +18,16 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProfileImageServiceImpl implements ProfileImageService {
     private final S3Service s3Service;
     private final MediaValidator mediaValidator;
-    private final BodyImageRepository bodyImageRepository;
     private final MediaFilesRepository mediaFilesRepository;
     private final UserRepository userRepository;
 
     @Override
     public BodyImageMainResponseDTO getMainProfileImage(long userId) {
-        BodyImageEntity entity = bodyImageRepository.findFirstUrlByUserIdOrderByRecordedAtDesc(userId);
-        if (entity == null) {
+        BodyImageMainResponseDTO responseDTO = mediaFilesRepository.findMainProfileImageByUserId(userId);
+        if (responseDTO.getImageUrl() == null) {
             return new BodyImageMainResponseDTO("https://fitu-bucket.s3.ap-northeast-2.amazonaws.com/fitu_default_image.png");
         }
-        String imageUrl = entity.getUrl();
-        return new BodyImageMainResponseDTO(imageUrl);
+        return responseDTO;
     }
 
     @Override
