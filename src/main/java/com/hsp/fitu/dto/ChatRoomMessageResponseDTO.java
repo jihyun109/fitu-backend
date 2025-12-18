453e765813d09260f8ea0588557de36a0cb8e1e4
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 15:58:49 +0900

    feat: getter, allargsconstructor 애너테이션 추가

--- src/main/java/com/hsp/fitu/dto/ChatRoomMessageResponseDTO.java
+++ src/main/java/com/hsp/fitu/dto/ChatRoomMessageResponseDTO.java
@@ -1,10 +1,12 @@
 package com.hsp.fitu.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@Builder
 public class ChatRoomMessageResponseDTO {
     private List<ChatMessage> messages;
 }
