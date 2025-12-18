9746bf00a3980fa5245721cff9525017c53f0f6b
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 20:05:55 +0900

    fix: 채팅방 메시지

--- src/main/java/com/hsp/fitu/repository/ChatMessageRepository.java
+++ src/main/java/com/hsp/fitu/repository/ChatMessageRepository.java
@@ -13,7 +13,7 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, 
             SELECT new com.hsp.fitu.dto.ChatMessage(u.name, cm.content, m.url, cm.createdAt, u.id)
             FROM ChatMessageEntity cm
             JOIN UserEntity u ON cm.senderId = u.id
-            JOIN MediaFilesEntity m ON u.profileImgId = m.id
+            LEFT JOIN MediaFilesEntity m ON u.profileImgId = m.id
             WHERE cm.chatRoomId = :chatRoomId
             ORDER BY cm.createdAt
             """)
