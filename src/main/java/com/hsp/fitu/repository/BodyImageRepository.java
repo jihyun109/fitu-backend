6572ad8c9d5c903dccfe5f2b29af67bbec805cb7
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 18:03:48 +0900

    fix: profile imgs 조회

--- src/main/java/com/hsp/fitu/repository/BodyImageRepository.java
+++ src/main/java/com/hsp/fitu/repository/BodyImageRepository.java
@@ -2,10 +2,7 @@ package com.hsp.fitu.repository;
 
 import com.hsp.fitu.entity.BodyImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,11 +10,4 @@ import java.util.List;
 public interface BodyImageRepository extends JpaRepository<BodyImageEntity, Long> {
 
     BodyImageEntity findFirstUrlByUserIdOrderByRecordedAtDesc(long userId);
-
-    @Modifying
-    @Query("DELETE FROM BodyImageEntity b WHERE b.url = :imageUrl")
-    @Transactional
-    void deleteByUrl(String imageUrl);
-
-    List<BodyImageEntity> findByUserIdOrderByRecordedAtDesc(long userId);
 }
--- src/main/java/com/hsp/fitu/repository/MediaFilesRepository.java
+++ src/main/java/com/hsp/fitu/repository/MediaFilesRepository.java
@@ -1,5 +1,6 @@
 package com.hsp.fitu.repository;
 
+import com.hsp.fitu.dto.ProfileImage;
 import com.hsp.fitu.entity.MediaFilesEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -7,8 +8,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MediaFilesRepository extends JpaRepository<MediaFilesEntity, Long> {
+    @Query("""
+            SELECT new com.hsp.fitu.dto.ProfileImage(m.url)
+            FROM MediaFilesEntity m
+            JOIN UserEntity u ON m.id = u.profileImgId
+            WHERE u.id = :userId
+            ORDER BY m.uploadedAt
+            """)
+    List<ProfileImage> findProfileImgsByUserId(long userId);
+
 
     @Modifying
     @Query("""
