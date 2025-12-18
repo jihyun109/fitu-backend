4ec45f64a466f4f8a2f70cb7e5fa5aaa90e5c6c8
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 15:13:33 +0900

    refactor: ChatRoomController 클래스명 ChatController로 수정

--- src/main/java/com/hsp/fitu/controller/ChatController.java
+++ src/main/java/com/hsp/fitu/controller/ChatController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/chat")
-public class ChatRoomController {
+public class ChatController {
 
     private final ChatRoomService chatRoomService;
 
