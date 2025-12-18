cef7d159bca8cffadf325b31b5860f4d64148ea0
jihyun109 <wlguswlgus989@gmail.com>
2025-12-18 15:59:09 +0900

    feat: 채팅방의 메시지 조회 api

--- src/main/java/com/hsp/fitu/repository/ChatMessageRepository.java
+++ src/main/java/com/hsp/fitu/repository/ChatMessageRepository.java
@@ -1,7 +1,21 @@
 package com.hsp.fitu.repository;
 
+import com.hsp.fitu.dto.ChatMessage;
 import com.hsp.fitu.entity.ChatMessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessageEntity, Long> {
+    @Query("""
+            SELECT new com.hsp.fitu.dto.ChatMessage(u.name, cm.content, m.url, cm.createdAt)
+            FROM ChatMessageEntity cm
+            JOIN UserEntity u ON cm.senderId = u.id
+            JOIN MediaFilesEntity m ON u.profileImgId = m.id
+            WHERE cm.chatRoomId = :chatRoomId
+            ORDER BY cm.createdAt
+            """)
+    List<ChatMessage> findChatMessagesByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
