2966d98524c33095ed5bc719bdf2ff6688d40e77
jihyun109 <wlguswlgus989@gmail.com>
2026-01-12 16:19:05 +0900

    feat: 무중단 배포 검증 테스트에 사용할 api

---
+++ src/main/java/com/hsp/fitu/controller/VerifyZeroDownTImeController.java
@@ -0,0 +1,25 @@
+package com.hsp.fitu.controller;
+
+import com.hsp.fitu.jwt.CustomUserDetails;
+import com.hsp.fitu.service.RankingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+public class VerifyZeroDownTImeController {
+    private final RankingService rankingService;
+
+    @GetMapping()
+    public ResponseEntity<String> getTotal500Ranking(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        rankingService.getTotal500Ranking(userDetails.getId());
+
+        return ResponseEntity.ok("Hello FitU V1");
+    }
+
+}
