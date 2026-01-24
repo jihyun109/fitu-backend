30fdbca02ce8fd3c9261b68be7a9ca41eb52d923
jihyun109 <wlguswlgus989@gmail.com>
2026-01-24 19:47:29 +0900

    refactor: 클레임을 파싱하고, Authentication 객체 생성 로직을 JwtAuthenticationFilter.java에서 분리

--- src/main/java/com/hsp/fitu/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/hsp/fitu/jwt/JwtAuthenticationFilter.java
@@ -8,15 +8,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -29,27 +25,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String token = jwtTokenService.extractTokenFromHeader(request);
         Claims claims = jwtTokenService.validateToken(token);
 
-        // 6. 클레임 파싱
-        Long userId = claims.get("userId", Long.class);
-        String role = (String) claims.get("role");
-        Number universityIdNumber = (Number) claims.get("universityId");
-        Long universityId = universityIdNumber != null ? universityIdNumber.longValue() : null;
-
-        // 7. SecurityContext 에 추가할 Authentication 인스턴스 생성
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        if (role != null && !role.isBlank()) {
-            authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
-        }
-
-        // 8. 인증 객체 생성
-        CustomUserDetails userDetails = new CustomUserDetails(userId, claims.getSubject(), authorities, universityId);
-
-        UsernamePasswordAuthentication authentication = new UsernamePasswordAuthentication(userDetails, null, authorities);
-
-        // 9. SecurityContext에 Authentication 객체 추가
+        // SecurityContext 에 Authentication 인스턴스 추가
         SecurityContextHolder.getContext()
-                .setAuthentication(authentication);
-        filterChain.doFilter(request, response);    // 필터 체인의 다음 필터 호룿
+                .setAuthentication(jwtTokenService.createAuthentication(claims));
+
+        filterChain.doFilter(request, response);
     }
 
     @Override
