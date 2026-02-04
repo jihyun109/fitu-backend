61be9dfe9742e1d03f673a17e00f5989754e0cd0
jihyun109 <wlguswlgus989@gmail.com>
2026-02-04 23:41:18 +0900

    refactor: SecurityConfig 중복된 권한 설정 제거
    POST /api/v1/** 요청에 대한 인증 설정은 anyRequest().authenticated()에 포함되므로 불필요한 코드를 삭제함.

--- src/main/java/com/hsp/fitu/config/SecurityConfig.java
+++ src/main/java/com/hsp/fitu/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import com.hsp.fitu.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -35,8 +34,6 @@ public class SecurityConfig {
                         .requestMatchers(SecurityConstants.PERMIT_ALL_MATCHERS
                                 .toArray(org.springframework.security.web.util.matcher.RequestMatcher[]::new))
                         .permitAll()
-
-                        .requestMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
