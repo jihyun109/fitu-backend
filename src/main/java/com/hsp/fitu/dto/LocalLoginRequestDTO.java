1a70c059db2f277cd456557636354fb89d34f6ec
jihyun109 <wlguswlgus989@gmail.com>
2026-02-21 17:28:42 +0900

    Reapply "feat: dev 환경 로컬 로그인 엔드포인트 추가"
    This reverts commit cb493be6e9422ad476a09d335bf34a8c1962e468.

---
+++ src/main/java/com/hsp/fitu/dto/LocalLoginRequestDTO.java
@@ -0,0 +1,10 @@
+package com.hsp.fitu.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LocalLoginRequestDTO {
+    private String email;
+}
